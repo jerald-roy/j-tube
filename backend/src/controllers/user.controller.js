7bd5e1b1166ec85459e39be2e6dd90a9cf6f4260
jerald-roy <jeraldroyj143@gmail.com>
2025-11-04 09:02:31 +0530

    hopefully final commit

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -147,7 +147,7 @@ const loginUser = asyncHandler(async (req, res) =>{
 
     const options = {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite:"none"
     }
 
